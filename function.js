8460e3e457cbfc0123af8194174a2ed127888cd4
Steve-hub-dot <58389374+Steve-hub-dot@users.noreply.github.com>
2023-07-01 21:27:40 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,11 +1,30 @@
-export function assignment(colors){
-    if(colors.length > 2){
-        return (colors.length-1)*2
-    }
-    else if(colors.length < 2){
+export function assignment(colors) {
+    if(colors.length < 2) {
         return 0;
     }
-    else{
+    else if(colors.length == 2) {
         return 1;
+    }
+    else{
+        let answer = 1;
+        
+        function factorial(n){
+            let factorial = 1;
+            for(i = n; i > 0; i--) {
+                factorial = factorial*i;
+            }
+            return factorial;
+        }
+
+        function nCr(n, r) {
+            return (factorial(n)/(factorial(r)*(factorial(n-r))))
+        }
+
+        for(i = 2; i < colors.length+1; i++) {
+            answer = answer + nCr(colors.length, i);
+        }
+        return answer;
+
+
     }
 }
